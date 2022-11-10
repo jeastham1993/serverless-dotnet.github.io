ccdadf9e4ae95142b03dfbca22edbc134d24e0d7
James Eastham <jamesuk@amazon.com>
2022-11-10 07:40:30 +0000

    Update deployment branch to main

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   organizationName: 'jeastham1993', // Usually your GitHub org/user name.
   projectName: 'serverlessdotnet.github.io', // Usually your repo name.
   trailingSlash: false,
+  deploymentBranch: 'main',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
