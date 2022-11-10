738f9bfd8b570739fb889f75bb338508142b2b88
James Eastham <jamesuk@amazon.com>
2022-11-10 07:39:53 +0000

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,8 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jeastham1993', // Usually your GitHub org/user name.
-  projectName: 'serverlessdotnetdocs', // Usually your repo name.
+  projectName: 'serverlessdotnet.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
